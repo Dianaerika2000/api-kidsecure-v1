e77d5a404dcaf7859f49acae8b247b64c1cd2aaf
dianaM <diana.montano@assuresoftbolivia.com>
2023-12-01 04:09:35 -0400

    Add modules

---
+++ src/nacionality/entities/nacionality.entity.ts
@@ -0,0 +1,26 @@
+import { Column, CreateDateColumn, DeleteDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn, ManyToMany, JoinTable, OneToMany } from "typeorm";
+import { User } from "src/users/entities/user.entity";
+import { IndividualUser } from "src/users/entities/individual-user.entity";
+
+@Entity() //convierte inmediatamente a una tabla
+export class Nacionality {
+    @PrimaryGeneratedColumn()
+    id:number;
+
+    @Column({  name: 'name', nullable: false})
+    name:string;
+
+    @CreateDateColumn({ name: 'created_at'})
+    createdAt: Date;
+
+    @UpdateDateColumn({ name: 'updated_at'})
+    updatedAt: Date;
+
+    @DeleteDateColumn({ name: 'deleted_at'})
+    deletedAt: Date;
+
+    @OneToMany(() => IndividualUser, (individualusers) => individualusers.nacionality)
+    individualusers: IndividualUser[]
+
+   
+}
